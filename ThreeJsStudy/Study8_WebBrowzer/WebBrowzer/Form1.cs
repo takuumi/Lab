32471d8e53222a4a9bdba565c46d992a0f7e9fe6
takuumi <satu_noone@hotmail.com>
2018-09-02 10:23:17 +0900

    Add files via upload

---
+++ ThreeJsStudy/Study8_WebBrowzer/WebBrowzer/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WebBrowzer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowserTest.Navigate(System.IO.Directory.GetCurrentDirectory() + "\\htmlTest\\HTML\\LD_LDP.html#LD");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webBrowserTest.Navigate(System.IO.Directory.GetCurrentDirectory() + "\\htmlTest\\HTML\\LD_LDP.html#LDP");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            webBrowserTest.Navigate(System.IO.Directory.GetCurrentDirectory() + "\\htmlTest\\ThreeJs\\study6_Parallel\\index7.html");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            webBrowserTest.Navigate(System.IO.Directory.GetCurrentDirectory() + "\\htmlTest\\ThreeJs\\study5_physijs\\index1.html");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            webBrowserTest.Navigate(System.IO.Directory.GetCurrentDirectory() + "\\htmlTest\\ThreeJs\\study7_JQuery\\index.html");
+        }
+
+
+
+    }
+}
